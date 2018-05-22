506bb155fbb8a37d408fe941bb3e2cb40be46e0f
Yoon Lee <yoon@brandboom.com>
2018-05-22 15:08:45 -0700

    initial commit

---
+++ XYZ-HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYZ-HelloWorld
+//
+//  Created by Yoon Lee on 5/22/18.
+//  Copyright © 2018 Yoon Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYZ-HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYZ-HelloWorld
+//
+//  Created by Yoon Lee on 5/22/18.
+//  Copyright © 2018 Yoon Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
