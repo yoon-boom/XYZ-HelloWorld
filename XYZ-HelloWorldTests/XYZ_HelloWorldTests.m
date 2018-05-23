5c997974e5fa2a18e075affc9b31305882e78346
Yoon Lee <yoon@brandboom.com>
2018-05-22 17:10:10 -0700

    Created Sample Unit Test

---
+++ XYZ-HelloWorldTests/XYZ_HelloWorldTests.m
@@ -0,0 +1,39 @@
+//
+//  XYZ_HelloWorldTests.m
+//  XYZ-HelloWorldTests
+//
+//  Created by Yoon Lee on 5/22/18.
+//  Copyright © 2018 Yoon Lee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XYZ_HelloWorldTests : XCTestCase
+
+@end
+
+@implementation XYZ_HelloWorldTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
